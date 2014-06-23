a68291661105a1149695054f57f8f215e2798fa8
Nick Johannsen <Nicholas.Johannsen@gmail.com>
2014-06-23 15:50:54 -0700

    Add License info and ReadMe

---
+++ README.md
@@ -0,0 +1,11 @@
+#JSON Rest API Sessions
+
+##License
+
+JSON Rest API Events is Copyright 2014 Volary Foundation and Contributors  
+
+JSON Rest API Events is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+JSON Rest API Events is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License along with JSON Rest API Events.  If not, see <http://www.gnu.org/licenses/>.
--- auth-api.php
+++ auth-api.php
@@ -17,3 +17,24 @@ function session_api_init( $server ) {
 }
 
 add_action( 'wp_json_server_before_serve', 'session_api_init' );
+
+//==========================================================================//
+// This file is part of JSON Rest API Sessions.                             //
+//                                                                          //
+// JSON Rest API Events is Copyright 2014 Volary Foundation and             //
+// Contributors                                                             //
+//                                                                          //
+// JSON Rest API Sessions is free software: you can redistribute it and/or  //
+// modify it under the terms of the GNU Affero General Public License as    //
+// published by the Free Software Foundation, either version 3 of the       //
+// License, or  at your option) any later version.                          //
+//                                                                          //
+// JSON Rest API Sessions is distributed in the hope that it will be useful,//
+// but  WITHOUT ANY WARRANTY; without even the implied warranty of          //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        //
+// Affero General Public License for more details.                          //
+//                                                                          //
+// You should have received a copy of the GNU Affero General Public         //
+// License along with JSON Rest API Events.  If not, see                    //
+// <http://www.gnu.org/licenses/>.                                          //
+//==========================================================================//
